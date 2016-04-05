5d0d790d706fea213316c9a73ccf23e80f237570
Allen <gebz456@163.com>
2016-04-05 17:37:02 +0800

    饼完善

--- eastng-common-echarts4j/src/main/java/com/eastng/common/echarts/Option.java
+++ eastng-common-echarts4j/src/main/java/com/eastng/common/echarts/Option.java
@@ -3,193 +3,288 @@ package com.eastng.common.echarts;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
 /**
  * Echarts4J
  * @author caojia
  *
  */
 public class Option {
-	
-	private Title title;
-	
-	private Tooltip tooltip;
-	
-	private Legend legend;
-	
-	private List<XAxis> xAxis;
-	
-	private List<Series> series;
-	
-	public static class Title{
-		private String text;
-		private String subtext;
-		public String getText() {
-			return text;
-		}
-		public void setText(String text) {
-			this.text = text;
-		}
-		public String getSubtext() {
-			return subtext;
-		}
-		public void setSubtext(String subtext) {
-			this.subtext = subtext;
-		}
-	}
-	
-	public class Tooltip{
-		private String trigger;
+    @JSONField(ordinal=1)
+    private Title title;
+    @JSONField(ordinal=2)
+    private Tooltip tooltip;
+    @JSONField(ordinal=3)
+    private Legend legend;
+    @JSONField(ordinal=4)
+    private List<XAxis> xAxis;
+    @JSONField(ordinal=5)
+    private List<Series> series;
+    
+    public static class Title{
+        private String text;
+        private String subtext;
+        private String x;
+        public String getText() {
+            return text;
+        }
+        public void setText(String text) {
+            this.text = text;
+        }
+        public String getSubtext() {
+            return subtext;
+        }
+        public void setSubtext(String subtext) {
+            this.subtext = subtext;
+        }
+        public String getX() {
+            return x;
+        }
+        public void setX(String x) {
+            this.x = x;
+        }
+        
+    }
+    
+    public class Tooltip{
+        private String trigger;
+        private String formatter;
 
-		public String getTrigger() {
-			return trigger;
-		}
+        public String getTrigger() {
+            return trigger;
+        }
 
-		public void setTrigger(String trigger) {
-			this.trigger = trigger;
-		}
-	}
-	
-	public static class Legend{
-		private List<String> data;
+        public void setTrigger(String trigger) {
+            this.trigger = trigger;
+        }
 
-		public List<String> getData() {
-			return data;
-		}
+        public String getFormatter() {
+            return formatter;
+        }
 
-		public void setData(List<String> data) {
-			this.data = data;
-		}
-	}
-	
-	public class XAxis{
-		private String type;
-		private Boolean boundaryGap;
-		private List<String> data;
-		public String getType() {
-			return type;
-		}
-		public void setType(String type) {
-			this.type = type;
-		}
-		public Boolean getBoundaryGap() {
-			return boundaryGap;
-		}
-		public void setBoundaryGap(Boolean boundaryGap) {
-			this.boundaryGap = boundaryGap;
-		}
-		public List<String> getData() {
-			return data;
-		}
-		public void setData(List<String> data) {
-			this.data = data;
-		}
-	}
-	
-	public static class Series{
-		private String name;
-		private String type;
-		private List<String> data;
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getType() {
-			return type;
-		}
-		public void setType(String type) {
-			this.type = type;
-		}
-		public List<String> getData() {
-			return data;
-		}
-		public void setData(List<String> data) {
-			this.data = data;
-		}
-	}
-	
-	public Title createTitle(){
-		if(this.title == null){
-			this.title = new Title();
-		}
-		return this.title;
-	}
-	
-	public Legend createLegend(){
-		if(this.legend == null){
-			this.legend = new Legend();
-		}
-		return this.legend;
-	}
-	
-	public Tooltip createTooltip(){
-		if(this.tooltip == null){
-			this.tooltip = new Tooltip();
-		}
-		return this.tooltip;
-	}
-	
-	public void addXAxis(XAxis xaxis){
-		this.createXAxis().add(xaxis);
-	}
+        public void setFormatter(String formatter) {
+            this.formatter = formatter;
+        }
+        
+    }
+    
+    public static class Legend{
+        private String orient;
+        private String left;
+        private List<String> data;
 
-	protected List<XAxis> createXAxis(){
-		if(this.xAxis == null){
-			xAxis = new ArrayList<Option.XAxis>();
-		}
-		return xAxis;
-	}
-	
-	public void addSeries(Series series){
-		this.createSeries().add(series);
-	}
-	
-	protected List<Series> createSeries(){
-		if(this.series == null){
-			this.series = new ArrayList<Series>();
-		}
-		return this.series;
-	}
+        /**
+         * @return the orient
+         */
+        public String getOrient() {
+            return orient;
+        }
 
-	public Title getTitle() {
-		return title;
-	}
+        /**
+         * @param orient the orient to set
+         */
+        public void setOrient(String orient) {
+            this.orient = orient;
+        }
+        
+        /**
+         * @return the left
+         */
+        public String getLeft() {
+            return left;
+        }
 
-	public void setTitle(Title title) {
-		this.title = title;
-	}
+        /**
+         * @param left the left to set
+         */
+        public void setLeft(String left) {
+            this.left = left;
+        }
 
-	public Tooltip getTooltip() {
-		return tooltip;
-	}
+        public List<String> getData() {
+            return data;
+        }
 
-	public void setTooltip(Tooltip tooltip) {
-		this.tooltip = tooltip;
-	}
+        public void setData(List<String> data) {
+            this.data = data;
+        }
+    }
+    
+    public class XAxis{
+        private String type;
+        private Boolean boundaryGap;
+        private List<String> data;
+        public String getType() {
+            return type;
+        }
+        public void setType(String type) {
+            this.type = type;
+        }
+        public Boolean getBoundaryGap() {
+            return boundaryGap;
+        }
+        public void setBoundaryGap(Boolean boundaryGap) {
+            this.boundaryGap = boundaryGap;
+        }
+        public List<String> getData() {
+            return data;
+        }
+        public void setData(List<String> data) {
+            this.data = data;
+        }
+    }
+    
+    public static class Series{
+        @JSONField(ordinal=1)
+        private String name;
+        @JSONField(ordinal=2)
+        private String type;
+        @JSONField(ordinal=3)
+        private String radius;
+        @JSONField(ordinal=4)
+        private List<Data> data;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getType() {
+            return type;
+        }
+        public void setType(String type) {
+            this.type = type;
+        }
+        public String getRadius() {
+            return radius;
+        }
+        public void setRadius(String radius) {
+            this.radius = radius;
+        }
+        public List<Data> getData() {
+            return data;
+        }
+        public void setData(List<Data> data) {
+            this.data = data;
+        }
+        
+        public List<Data> createData(){
+            if(this.data == null){
+                this.data = new ArrayList<Option.Series.Data>();
+            }
+            return this.data;
+        }
+        
+        public void addData(Data data){
+            this.createData().add(data);
+        }
+        
+        public static class Data{
+            private Integer value;
+            
+            private String name;
 
-	public Legend getLegend() {
-		return legend;
-	}
+            public Integer getValue() {
+                return value;
+            }
 
-	public void setLegend(Legend legend) {
-		this.legend = legend;
-	}
+            public void setValue(Integer value) {
+                this.value = value;
+            }
 
-	public List<XAxis> getxAxis() {
-		return xAxis;
-	}
+            public String getName() {
+                return name;
+            }
 
-	public void setxAxis(List<XAxis> xAxis) {
-		this.xAxis = xAxis;
-	}
+            public void setName(String name) {
+                this.name = name;
+            }
+            
+        }
+    }
+    
+    public Title createTitle(){
+        if(this.title == null){
+            this.title = new Title();
+        }
+        return this.title;
+    }
+    
+    public Legend createLegend(){
+        if(this.legend == null){
+            this.legend = new Legend();
+        }
+        return this.legend;
+    }
+    
+    public Tooltip createTooltip(){
+        if(this.tooltip == null){
+            this.tooltip = new Tooltip();
+        }
+        return this.tooltip;
+    }
+    
+    public void addXAxis(XAxis xaxis){
+        this.createXAxis().add(xaxis);
+    }
 
-	public List<Series> getSeries() {
-		return series;
-	}
+    protected List<XAxis> createXAxis(){
+        if(this.xAxis == null){
+            xAxis = new ArrayList<Option.XAxis>();
+        }
+        return xAxis;
+    }
+    
+    public void addSeries(Series series){
+        this.createSeries().add(series);
+    }
+    
+    protected List<Series> createSeries(){
+        if(this.series == null){
+            this.series = new ArrayList<Series>();
+        }
+        return this.series;
+    }
 
-	public void setSeries(List<Series> series) {
-		this.series = series;
-	}
-	
+    public Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+    public Tooltip getTooltip() {
+        return tooltip;
+    }
+
+    public void setTooltip(Tooltip tooltip) {
+        this.tooltip = tooltip;
+    }
+
+    public Legend getLegend() {
+        return legend;
+    }
+
+    public void setLegend(Legend legend) {
+        this.legend = legend;
+    }
+
+    public List<XAxis> getxAxis() {
+        return xAxis;
+    }
+
+    public void setxAxis(List<XAxis> xAxis) {
+        this.xAxis = xAxis;
+    }
+
+    public List<Series> getSeries() {
+        return series;
+    }
+
+    public void setSeries(List<Series> series) {
+        this.series = series;
+    }
+    
 }
--- eastng-common-echarts4j/src/test/java/com/eastng/common/echarts/Test.java
+++ eastng-common-echarts4j/src/test/java/com/eastng/common/echarts/Test.java
@@ -1,15 +1,53 @@
 package com.eastng.common.echarts;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.alibaba.fastjson.JSON;
+import com.eastng.common.echarts.Option.Legend;
+import com.eastng.common.echarts.Option.Series;
 import com.eastng.common.echarts.Option.Title;
 import com.eastng.common.echarts.Option.Tooltip;
+import com.eastng.common.echarts.Option.Series.Data;
 
 public class Test{
     
     public static void main(String[] args) {
-        Option option = new Option();
-        Title title= option.createTitle();
-        Tooltip tooltip = option.createTooltip();
+    	Option option = new Option();
+		Title title = option.createTitle();
+		//标题
+		title.setText("近10场交锋");
+		//标题居中
+		title.setX("center");
+		Tooltip tooltip = option.createTooltip();
+		tooltip.setTrigger("item");
+		tooltip.setFormatter("{a} <br/>{b} : {c} ({d}%)");
+		
+		Legend legend = option.createLegend();
+		legend.setOrient("vertical");
+		List<String> data = new ArrayList<String>();
+		data.add("胜");
+		data.add("平");
+		data.add("负");
+		legend.setData(data);
+		
+		Series series = new Series();
+		series.setName("胜平负比");
+		series.setType("pie");
+		
+		Data data2 = new Data();
+		data2.setName("胜");
+		data2.setValue(7);
+		series.addData(data2);
+		Data data3 = new Data();
+		data3.setName("平");
+		data3.setValue(2);
+		series.addData(data3);
+		Data data4 = new Data();
+		data4.setName("负");
+		data4.setValue(1);
+		series.addData(data4);
+		option.addSeries(series);
         String s = JSON.toJSONString(option, true);
         System.out.println(s);
     }
